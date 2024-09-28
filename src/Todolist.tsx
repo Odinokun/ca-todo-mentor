fc0e1ccd26e924d68737ad5df3e525224b405096
odinokun <odinokun@gmail.com>
2024-09-28 11:24:20 -0400

    Refactor App component to use Todolist component; update styles

---
+++ src/Todolist.tsx
@@ -0,0 +1,31 @@
+type PropsType = {
+  title: string;
+};
+
+export const Todolist = ({ title }: PropsType) => {
+  return (
+    <div className='todolist'>
+      <h3>{title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type='checkbox' checked={true} /> <span>HTML&CSS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={true} /> <span>JS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={false} /> <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+};
