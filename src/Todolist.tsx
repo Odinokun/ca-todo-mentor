fd38092e2095e92f62491b1ad8725b3dfa9e3884
odinokun <odinokun@gmail.com>
2024-11-16 15:42:10 -0500

    Refactor Todolist component; invert conditional rendering for task list and empty state message

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -38,7 +38,7 @@ export const Todolist: FC<PropsType> = ({ title, tasks, ...restProps }) => {
         <Button name='Completed' onClick={() => {}} />
       </div>
 
-      {!tasks.length ? <div>You have no tasks </div> : <ul>{tasksList}</ul>}
+      {tasks.length ? <ul>{tasksList}</ul> : <div>You have no tasks </div>}
     </div>
   );
 };
